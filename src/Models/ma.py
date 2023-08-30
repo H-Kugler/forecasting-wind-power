63420726d9c63d9ca47e4ee89e4ce772b80995d1
H-Kugler <harald.kugler@student.uni-tuebingen.de>
2023-08-30 19:00:20 +0200

    MA can now be used in pipeline + hyperparameteropt

--- src/Models/ma.py
+++ src/Models/ma.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Union
 import numpy as np
 import pandas as pd
 
@@ -8,37 +8,40 @@ from src.Models.basemodel import Basemodel
 class MovingAverage(Basemodel):
     def __init__(
         self,
-        data: pd.DataFrame,
-        horizon: Literal["10min", "Hourly", "Daily"],
-        window_size: int,
         discount: float = 1.0,
     ):
-        super().__init__(data, horizon, window_size)
         self.discount = discount
 
-    def fit(self, test_start, test_end):
+    def fit(self, X: pd.DataFrame, y: Union[pd.Series, str]):
         """
         Fits the model to the given data.
         :param test_start: The start of the test set
         :param test_end: The end of the test set
         """
-        super().fit(test_start, test_end)
-        self.X_test = self.X_test[
-            [
-                f"Power (kW) (time -{i})"
-                for i in range(
-                    self.time_steps_ahead, self.time_steps_ahead + self.window_size
-                )
-            ]
-        ]
+        if isinstance(y, str):
+            self.target_var = y
+        else:
+            self.target_var = y.name
 
-    def predict(self):
+        # Check if target variable is substring of at least one column in X
+        # raise error
+        if not X.columns.str.contains(self.target_var, regex=False).any():
+            raise ValueError("Target variable not found in X")
+
+    def predict(self, X: pd.DataFrame):
         """
         Predicts the next Power output for the given data time_steps_ahead into the future.
         :return: A list-like object of predictions
         """
-        if self.X_test is None:
-            raise ValueError("Model has not been fitted yet.")
-        discounts = np.array([self.discount**i for i in range(self.window_size)])
-        predictions = self.X_test.multiply(discounts).sum(axis=1).div(discounts.sum())
+        # select columns where target variable is substring of column name
+        cols = X.columns[X.columns.str.contains(self.target_var, regex=False)]
+        window_size = len(cols)
+
+        if window_size == 0:
+            raise ValueError("No columns match target variable")
+
+        # compute predictions
+        X = X[cols]
+        discounts = np.array([self.discount**i for i in range(window_size)])
+        predictions = X.multiply(discounts).sum(axis=1).div(discounts.sum())
         return predictions.to_numpy()
