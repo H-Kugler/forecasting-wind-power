7a310cc8deda13e02b773085f09b124441f45e25
H-Kugler <harald.kugler@student.uni-tuebingen.de>
2023-08-31 15:15:29 +0200

    looked into testing

---
+++ tests/unit/DataHandling/test_preprocessing.py
@@ -0,0 +1,8 @@
+from src.DataHandling.preprocessing import *
+import pandas as pd
+import os
+
+
+def test_train_test_split():
+    pass
+
---
+++ tests/unit/test_imports.py
@@ -0,0 +1,5 @@
+def test_import_DataHandling():
+    import src.DataHandling
+
+def test_import_Models():
+    import src.Models
