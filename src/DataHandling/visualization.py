9b3d9a15ff371eb81b1b100794477eac191ba7a3
H-Kugler <harald.kugler@student.uni-tuebingen.de>
2023-09-10 18:59:46 +0200

    plotting MovingAverage

--- src/DataHandling/visualization.py
+++ src/DataHandling/visualization.py
@@ -5,6 +5,81 @@ import seaborn as sns
 from typing import List, Union, Tuple
 
 
+def plot_predictions(
+    predictions: pd.DataFrame, y_true: pd.Series, save_path: str = None
+):
+    """
+    Plot predictions for each horizon in predictions.
+    :param predictions: DataFrame with predictions.
+                        Columns are horizons / different models, index is time.
+    :param y_true: Series with true values. Index is time.
+    :param save_path: Path to save the plot
+    """
+    horizons = predictions.columns
+    _, n = predictions.shape
+
+    # Check if predictions and y_true have the same index
+    if y_true is not None:
+        assert predictions.index.equals(y_true.index)
+
+    _, axs = plt.subplots(n, 1, figsize=(5 * n, 10), sharex=True)
+    for i, horizon in enumerate(horizons):
+        if y_true is not None:
+            sns.lineplot(
+                x=y_true.index,
+                y=y_true,
+                ax=axs[i],
+                label="True",
+            )
+        sns.lineplot(
+            x=predictions.index,
+            y=predictions[horizon],
+            ax=axs[i],
+            label=f"Predictions ({horizon})",
+        )
+        axs[i].set_ylabel("Power (kW)")
+        axs[i].set_title(f"Predictions for horizon '{horizon}'")
+        axs[i].legend()
+
+    if save_path is not None:
+        plt.savefig(save_path)
+    plt.show()
+
+
+def plot_gridsearch_results(
+    df: pd.DataFrame, x: str, hue: str = None, save_path: str = None
+):
+    """
+    Plot the results of a gridsearch in a barplot.
+    :param df: Gridsearch results in a dataframe
+    """
+    # extract the columns that store the parameters of the model
+    params = df.columns[df.columns.str.contains("param_")]
+    # extract the columns that store the results of the model
+    cols = df.columns[df.columns.str.contains("split")]
+    df = -df.set_index(list(params))[cols]
+    df = pd.melt(
+        df.reset_index(),
+        id_vars=params,
+        value_vars=cols,
+        var_name="split",
+        value_name="RMSE",
+    )
+    plot = sns.catplot(
+        data=df,
+        x=x,
+        y="RMSE",
+        hue=hue,
+        col="param_st__horizon",
+        kind="bar",
+    )
+    plot.fig.subplots_adjust(top=0.9)
+    plot.fig.suptitle("RMSE for different hyperparameter settings")
+    if save_path is not None:
+        plt.savefig(save_path)
+    plt.show()
+
+
 def plot_var_over_time(
     column_name: Union[str, List[str]],
     data: pd.DataFrame,
